91eb27ce4d06813bfc02d3afa498451250473a23
Lucca Fraser <lucca.fraser@gmail.com>
2022-01-10 15:07:13 -0400

    mathjax config

--- plog.pl
+++ plog.pl
@@ -327,6 +327,7 @@ user:head(my_style, Head) -->
     html(head([meta([name=viewport,
                      content='width=device-width, initial-scale=1.0']),
                \html_requires(scripts('es5/tex-mml-chtml.js')),
+               \html_requires(scripts('MathJaxConfig.js')),
                \html_requires(css('stylesheet.css')),
                Head])).
 
